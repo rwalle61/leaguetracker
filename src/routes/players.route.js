46a4074b625e490aea1305e17a3befcda4af532a
Richard Waller <Richard.Waller@ibm.com>
2019-04-18 17:18:36 +0100

    remove obsolete swagger comments from route files

--- src/routes/players.route.js
+++ src/routes/players.route.js
@@ -4,59 +4,11 @@ const { playersController } = require('../controllers');
 
 const router = express.Router();
 
-/**
- * @swagger
- * /players:
- *    get:
- *      description: This should return all players
- *      tags:
- *          - users
- *      summary: A list of players
- *      responses:
- *          '200':
- *              description: player info
- *              content:
- *                  application/json:
- *                      schema:
- *                          type: array
- *                          items:
- *                              $ref: '#/components/schemas/Player'
- *          '400':
- *              description: bad input parameter
- *          '404':
- *              $ref: '#/components/responses/404NotFound'
- */
 router.get('/', async (req, res) => {
     const players = await playersController.getPlayers();
     res.status(200).send(players);
 });
 
-/**
- * @swagger
- * /players/{id}:
- *    get:
- *      tags:
- *          - users
- *      summary: A single player
- *      parameters:
- *          - in: path
- *            name: id
- *            # description: pass a required player id
- *            required: true
- *            schema:
- *                $ref: '#/components/schemas/ID'
- *      responses:
- *          '200':
- *              description: player info
- *              content:
- *                  application/json:
- *                      schema:
- *                          type: array
- *                          items:
- *                              $ref: '#/components/schemas/Player'
- *          '404':
- *              $ref: '#/components/responses/404NotFound'
- */
 router.get('/:id', async (req, res) => {
     const playerName = req.params.id;
     const player = await playersController.getPlayer(playerName);
--- src/routes/root.route.js
+++ src/routes/root.route.js
@@ -2,17 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-/**
- * @swagger
- * /:
- *    get:
- *      tags:
- *          - users
- *      summary: health endpoint
- *      responses:
- *          '200':
- *              description: app running
- */
 router.get('/', (req, res) => {
     res.sendStatus(200);
 });
