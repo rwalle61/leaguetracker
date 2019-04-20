c79394f87220f22679a8f736e3c8a024a917a63d
Richard Waller <Richard.Waller@ibm.com>
2019-04-20 13:33:50 +0100

    automate validation of seasons API using openApi 3 spec

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,9 @@
 
+const path = require('path');
+
 const env = process.env.NODE_ENV || 'development';
 
-module.exports = require(`./${env}`); // eslint-disable-line import/no-dynamic-require
+module.exports = {
+    ...require(`./${env}`), // eslint-disable-line import/no-dynamic-require
+    pathToDocsDir: path.join(__dirname, '../../public/docs'),
+};
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,17 @@
 function handleErrors(err, req, res, next) {
-    err.statusCode = err.statusCode || 500;
-    res.status(err.statusCode).send(err.message);
+    // logError(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).send(err.message);
+}
+
+function logError(err) {
+    console.log({
+        error: {
+            name: err.name,
+            message: err.message,
+            // data: err.data,
+        },
+    });
 }
 
 module.exports = {
