d461a727185ae748e4989567c36d27f378512bde
Richard Waller <Richard.Waller@ibm.com>
2019-04-27 18:37:24 +0100

    test & tidy reqValidator.js

--- src/routes/seasons.route.js
+++ src/routes/seasons.route.js
@@ -5,18 +5,14 @@ const { validateReq } = require('../middleware/reqValidator');
 
 const router = express.Router();
 
-router.post('/',
-    validateReq('post', '/seasons'),
-    async (req, res) => {
-        const detailsOfCreatedSeason = await seasonsController.createSeason(req.body);
-        res.status(201).send(detailsOfCreatedSeason);
-    });
+router.post('/', validateReq, async (req, res) => {
+    const detailsOfCreatedSeason = await seasonsController.createSeason(req.body);
+    res.status(201).send(detailsOfCreatedSeason);
+});
 
-router.put('/',
-    validateReq('put', '/seasons'),
-    async (req, res) => {
-        const detailsOfUpdatedSeason = await seasonsController.updateSeason(req.body);
-        res.status(200).send(detailsOfUpdatedSeason);
-    });
+router.put('/', validateReq, async (req, res) => {
+    const detailsOfUpdatedSeason = await seasonsController.updateSeason(req.body);
+    res.status(200).send(detailsOfUpdatedSeason);
+});
 
 module.exports = router;
