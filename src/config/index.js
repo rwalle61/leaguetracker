35b0aedf3b04b63eb6a53a4d3af34a46a6fd9639
Richard Waller <Richard.Waller@ibm.com>
2019-04-27 14:58:18 +0100

    auto-trigger err logging via NODE_ENV=development

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,15 @@ const path = require('path');
 
 const env = process.env.NODE_ENV || 'development';
 
+function determineLogLevel(env) {
+    if (env === 'production' || env === 'staging') {
+        return 'info';
+    }
+    return 'debug';
+}
+
 module.exports = {
     ...require(`./${env}`), // eslint-disable-line import/no-dynamic-require
     pathToDocsDir: path.join(__dirname, '../../public/docs'),
+    logLevel: determineLogLevel(env),
 };
