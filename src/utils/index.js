c10b16515d3b9301ea861da4c995f8259e1da1d3
Richard Waller <Richard.Waller@ibm.com>
2019-05-07 19:48:32 +0100

    tidy utils comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,13 +4,15 @@ const fs = require('fs-extra');
 
 const { pathToDocsDir, logLevel } = require('../config');
 
+/**
+ * Writes/updates our openApi2.json from our existing OpenApi3 spec
+ */
 async function syncOpenApi2and3Docs() {
-    // write an OpenApi 2 Doc from our existing OpenApi 3 Doc
     const convertedJson = await Converter.convert({
         from: 'openapi_3',
         to: 'swagger_2',
         source: path.join(pathToDocsDir, 'openApi3.yml'),
-    })
+    });
 
     const options = { order: 'openapi' };
     await fs.writeFile(
