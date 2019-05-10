395b10acc29f4fe2c68a0e177c5dff034d890fad
Richard Waller <Richard.Waller@ibm.com>
2019-05-10 22:02:13 +0100

    update comment

--- stryker.conf.js
+++ stryker.conf.js
@@ -14,7 +14,8 @@ module.exports = function(config) {
         reporters: ['clear-text', 'dots'], // see https://stryker-mutator.io/stryker/plugins#reporters
         testRunner: 'mocha',
         mochaOptions: {
-            // skip api tests because (1) ports clash, and (2) unit tests should catch everything
+            // skip api tests because (1) ports clash, and (2) unit tests should catch everything.
+            // remember to include (above) any new files that should be unit-tested
             spec: ['./test/unit/**/*.test.js'],
         },
         transpilers: [],
