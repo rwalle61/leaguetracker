8021abcac84053869b2c50f7a6638cdafdb55316
Richard Waller <Richard.Waller@ibm.com>
2019-05-12 16:13:36 +0100

    try setting pool

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,15 @@ module.exports = {
     client: 'pg',
     version: '7.2',
     connection: {
-        user: process.env.DB_USER, 
+        user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_DATABASE,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
+    },
+    pool: {
+        // min: 0,
+        // max: 12,
+        propagateCreateError: false,
     },
 };
